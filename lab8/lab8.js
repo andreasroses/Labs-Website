cb4b1ddc460f6c44f38e890c24e291f812c8c2fb
Andrea Mendez <andreacelestem11@gmail.com>
2023-10-16 13:54:11 -0500

    implemented javascript php ajax

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,4 +1,17 @@
+const button = document.getElementById("submit");
+const p = getElementById("responseString");
+button.addEventListener("click", function () {
+  getDataFromForm();
+});
 
 function getDataFromForm() {
-  alert("it worked!");
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "ajax.php", true);
+  xhr.onload() = function(){
+    if(xhr.status === 200){
+      p.textContent = xhr.responseText;
+    }
+  }
 }
