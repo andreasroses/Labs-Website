00b1b24a31e69ce5fbf57a0d48f4cf8fba445fca
Andrea Mendez <andreacelestem11@gmail.com>
2023-10-16 16:35:26 -0500

    debug lines

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -7,6 +7,8 @@ button.addEventListener("click", function () {
 function getDataFromForm() {
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
+  console.log("First Name:", fname);
+  console.log("Last Name:", lname);
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "ajax.php", true);
   xhr.onload() = function(){
