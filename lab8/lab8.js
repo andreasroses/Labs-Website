f22c44f4586491f20145d9bfe73178b62f09528d
Andrea Mendez <andreacelestem11@gmail.com>
2023-10-21 17:55:17 -0500

    added runAjax()

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function getDataFromForm() {
-  const p = document.getElementById("responseString");
+  
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
+  runAjax(fname,lname);
+}
+
+function runAjax(fname,lname){
+  const p = document.getElementById("responseString");
   const queryString = `?fname=${encodeURIComponent(fname)}&lname=${encodeURIComponent(lname)}`;
   var url = "ajax.php" + queryString;
   var xhr = new XMLHttpRequest();
