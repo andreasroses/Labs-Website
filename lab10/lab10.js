2be122bd9536593e573421ef96566a162df50fd1
Andrea Mendez <44706114+andreasroses@users.noreply.github.com>
2023-11-04 21:21:55 -0500

    changed show_data to GET req

--- lab10/lab10.js
+++ lab10/lab10.js
@@ -16,4 +16,21 @@ function runAjax(form){
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "save_data.php", true);
     xhr.send(formData);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Load show_data.php here or perform any other actions
+            loadShowData();
+        }
+    };
+}
+
+function loadShowData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "show_data.php", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector("#table-container").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
 }
--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,14 +1,17 @@
 <?php
-$lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$parsedFile = array();
-foreach($lines as $line){
-    list($showfname,$showlname) = explode(' ',$line,2);
-    $parsedFile[] = array('showfname' => $showfname, 'showlname' => $showlname);
+if ($_SERVER["REQUEST_METHOD"] === "GET"){
+    $file = "/home/site/priv/userInfo.txt";
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $parsedFile = array();
+    foreach($lines as $line){
+        list($fname,$lname) = explode(' ',$line,2);
+        $parsedFile[] = array('fname' => $fname, 'lname' => $lname);
+    }
+    echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
+    foreach($parsedFile as $entry){
+        echo "<tr><td>" . $entry['fname'] . "</td>";
+        echo "<td>" . $entry['lname'] . "</td></tr>";
+    }
+    echo "</table>";
 }
-echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
-foreach($parsedFile as $entry){
-    echo "<tr><td>" . $entry['showfname'] . "</td>";
-    echo "<td>" . $entry['showlname'] . "</td></tr>";
-}
-echo "</table>";
 ?>
