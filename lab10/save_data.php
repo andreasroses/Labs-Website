2563f8c413efa866561b101ad423cab2a719844b
Andrea Mendez <44706114+andreasroses@users.noreply.github.com>
2023-11-04 21:10:26 -0500

    new file path

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
-    $file = "D:/home/site/priv/userInfo.txt";
+    $file = "/home/site/priv/userInfo.txt";
     $data = "$fname $lname\n";
     file_put_contents($file, $data, FILE_APPEND);
     include('show_data.php');
