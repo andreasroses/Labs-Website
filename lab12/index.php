78bbd9d4349fc6ba4efc9d17fd940c8e229adfcf
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-21 14:43:11 -0600

    setting cookie for my subdomain

--- lab12/index.php
+++ lab12/index.php
@@ -16,7 +16,8 @@
     <h1 class="align-center">Cookies</h1>
     <br />
     <?php
-
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     if (isset($_COOKIE["CS4413"]) && $_COOKIE["CS4413"] > time()) {
         // The cookie is already set and has not expired
         echo "<p>CS4413 cookie is already set. Session ID: {$_COOKIE["CS4413"]}, Expiration: " . date("Y-m-d H:i:s", $_COOKIE["CS4413"]) . "</p>";
@@ -24,7 +25,7 @@
         // Set a new cookie
         $sessionID = uniqid();
         $expirationTime = time() + 36000;  // 10 hours
-        setcookie("CS4413", $sessionID, $expirationTime, "/");
+        setcookie("CS4413", $sessionID, $expirationTime, "/","ara-webtech.azurewebsites.net");
         echo "<p>CS4413 cookie is set. Session ID: $sessionID, Expiration: " . date("Y-m-d H:i:s", $expirationTime) . "</p>";
     
         // Debugging: Output all cookies
