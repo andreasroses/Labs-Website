d53e74abf224814c6c51d27513a4eaa8cb406652
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-21 14:56:06 -0600

    php script before HTML this time

--- lab12/index.php
+++ lab12/index.php
@@ -1,3 +1,20 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    if (isset($_COOKIE["CS4413"]) &&strtotime($_COOKIE["CS4413"]) > time()) {
+        // The cookie is already set and has not expired
+        echo "<p>CS4413 cookie is already set. Session ID: {$_COOKIE["CS4413"]}, Expiration: " . date("Y-m-d H:i:s", $_COOKIE["CS4413"]) . "</p>";
+    } else {
+        // Set a new cookie
+        $sessionID = uniqid();
+        $expirationTime = time() + 36000;  // 10 hours
+        setcookie("CS4413", $sessionID, $expirationTime, "/","ara-webtech.azurewebsites.net");
+        echo "<p>CS4413 cookie is set. Session ID: $sessionID, Expiration: " . date("Y-m-d H:i:s", $expirationTime) . "</p>";
+    
+        // Debugging: Output all cookies
+        echo "<p>All Cookies: " . json_encode($_COOKIE) . "</p>";
+    }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -15,23 +32,7 @@
 
     <h1 class="align-center">Cookies</h1>
     <br />
-    <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    if (isset($_COOKIE["CS4413"]) &&strtotime($_COOKIE["CS4413"]) > time()) {
-        // The cookie is already set and has not expired
-        echo "<p>CS4413 cookie is already set. Session ID: {$_COOKIE["CS4413"]}, Expiration: " . date("Y-m-d H:i:s", $_COOKIE["CS4413"]) . "</p>";
-    } else {
-        // Set a new cookie
-        $sessionID = uniqid();
-        $expirationTime = time() + 36000;  // 10 hours
-        setcookie("CS4413", $sessionID, $expirationTime, "/","ara-webtech.azurewebsites.net");
-        echo "<p>CS4413 cookie is set. Session ID: $sessionID, Expiration: " . date("Y-m-d H:i:s", $expirationTime) . "</p>";
     
-        // Debugging: Output all cookies
-        echo "<p>All Cookies: " . json_encode($_COOKIE) . "</p>";
-    }
-?>
 
 </body>
 
