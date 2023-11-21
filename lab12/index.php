37492ee5da7e5b4a3f0096d327f96799344ef7ad
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-21 13:40:51 -0600

    error checking lab12

--- lab12/index.php
+++ lab12/index.php
@@ -2,27 +2,31 @@
 <html>
 
 <head>
-<title> Cookies </title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans&family=Merienda:wght@800&family=Quicksand:wght@500;600&display=swap" rel="stylesheet">
-<link rel = "stylesheet" type = "text/css" href = "mystyle.css" />
+    <title> Cookies </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inclusive+Sans&family=Merienda:wght@800&family=Quicksand:wght@500;600&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="mystyle.css" />
 </head>
+
 <body>
 
-<h1 class = "align-center">Cookies</h1>
-<br />
-<?php
+    <h1 class="align-center">Cookies</h1>
+    <br />
+    <?php
     session_start();
 
-if (isset($_COOKIE["CS4413"])) {
-    echo "<p>CS4413 cookie is already set. Session ID: {$_COOKIE["CS4413"]}</p>";
-} else {
-    $sessionID = uniqid();
-    setcookie("CS4413", $sessionID, time() + 3600, "/");
-    echo "<p>CS4413 cookie is set. Session ID: $sessionID</p>";
-}
+    if (isset($_COOKIE["CS4413"])) {
+        echo "<p>CS4413 cookie is already set. Session ID: {$_COOKIE["CS4413"]}, Expiration: " . date("Y-m-d H:i:s", $_COOKIE["CS4413"]) . "</p>";
+    } else {
+        $sessionID = uniqid();
+        setcookie("CS4413", $sessionID, time() + 3600, "/");
+        echo "<p>CS4413 cookie is set. Session ID: $sessionID, Expiration: " . date("Y-m-d H:i:s", time() + 3600) . "</p>";
+    }
 ?>
 
 </body>
+
 </html>
